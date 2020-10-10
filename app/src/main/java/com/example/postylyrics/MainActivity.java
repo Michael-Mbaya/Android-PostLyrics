1d022c0852012d30cd2dd394c282d01a1a4b3410
Michael-Mbaya <gikundamike@gmail.com>
2020-10-10 18:29:53 +0300

    activity_search added

--- app/src/main/java/com/example/postylyrics/MainActivity.java
+++ app/src/main/java/com/example/postylyrics/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.postylyrics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.searchButton) Button mSearchButton;
+    @BindView(R.id.editTexSearch) EditText mEditTextSearch;
+//    @BindView(R.id.searchButton) Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mSearchButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        if(v==mSearchButton){
+            String searchMusic =mEditTextSearch.getText().toString();
+            Intent intent = new Intent(MainActivity.this,SearchActivity.class);
+            //pass data with intent extras
+            intent.putExtra("searchMusic", searchMusic);
+            //go to search activity
+            startActivity(intent);
+        }
+    }
+
+
 }
